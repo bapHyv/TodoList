dc644e3b46af6d71fe5fc33da0243693cbe50796
baphyv <baptiste.hyver@gmail.com>
2020-01-22 16:57:12 +0100

    implemented the delete functionality so the user can delete the todos

--- src/components/ManageMyTodos.js
+++ src/components/ManageMyTodos.js
@@ -1,11 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import TodosItem from './TodosItem';
 
 export default class ManageMyTodos extends Component {
-    render() {
-        return (
-            <div>
-                MANAGE MY TODOS
-            </div>
-        )
-    }
+	constructor() {
+		super();
+		this.state = {
+			data: []
+		};
+	}
+
+	handleClickDelete = id => {
+		axios.delete(`/tasks/${id}`);
+		console.log(id);
+		axios.get('/tasks').then(data => {
+			this.setState({ data: data.data });
+		});
+	};
+
+	componentDidMount() {
+		axios.get('/tasks').then(data => {
+			this.setState({ data: data.data });
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Manage My Todos</h1>
+				<table>
+					<thead>
+						<tr>
+							<th>todo</th>
+							<th>note</th>
+							<th>beggining</th>
+							<th>dead line</th>
+							<th>time left</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.state.data.map(task => {
+							return (
+								<TodosItem
+									key={task.id}
+									todo={task}
+									handleClickDelete={this.handleClickDelete}
+								/>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
+		);
+	}
 }
---
+++ src/components/TodosItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const TodosItem = ({ todo, handleClickDelete }) => {
+	const {
+		id,
+		task,
+		note,
+		beggining,
+		deadline,
+		timeleft
+	} = todo;
+	return (
+		<tr>
+			<td>{task}</td>
+			<td>{note}</td>
+			<td>{beggining}</td>
+			<td>{deadline}</td>
+			<td>{timeleft}</td>
+			<td>
+				<button onClick={() => handleClickDelete(id)}>Delete</button>
+			</td>
+			<td>
+				<button>Modify</button>
+			</td>
+		</tr>
+	);
+};
+
+export default TodosItem;
